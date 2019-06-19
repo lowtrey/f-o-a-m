29c3e7500d323a7ff0fac883101d68a97acbe477
Q. Price <priceqp@gmail.com>
2019-06-19 14:31:33 -0500

    Add header and footer partials

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ app.use(express.static(__dirname + "/public"));
 app.get('/', function(req, res){
     res.render('home');
 });
+app.get('/about', function(req, res){
+    res.render('about');
+});
+app.get('/outreach', function(req, res){
+    res.render('outreach');
+});
+app.get('/events', function(req, res){
+    res.render('events');
+});
+app.get('/donate', function(req, res){
+    res.render('donate');
+});
+app.get('/contact', function(req, res){
+    res.render('contact');
+});
 
 
 
